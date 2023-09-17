fdf0ae42553930623357bf67209d55a35fcb0ec2
Wallison (DESKTOP) <wallison-bruno@hotmail.com>
2023-09-17 18:56:58 -0300

    hook: refactor

--- src/hook/useOccurrence.tsx
+++ src/hook/useOccurrence.tsx
@@ -6,152 +6,45 @@ import React, {
   useState,
 } from "react";
 
-interface AtributesType {
-  name: string;
-  url: string;
+
+export interface Information{
+  
 }
 
-interface Type {
-  type: AtributesType;
+export interface Patrolling{
+ 
 }
 
-export interface PokemonDTO {
-  img: string;
-  name: string;
-  id: number;
-  type: Type[];
-  isFavorite: boolean;
+export interface Seizures{
+ 
 }
 
-interface PokemonProviderProps {
+interface RegisterProviderProps {
   children: ReactNode;
 }
 
-interface PokemonData {
-  listPokemon: PokemonDTO[];
-  listLikedPokemon: PokemonDTO[];
-  listSearchPokemon: PokemonDTO[];
-  handleSetListPokemon: (listPokemon: PokemonDTO[]) => void;
-  handleAddListPokemon: (pokemon: PokemonDTO) => void;
-  handleRemoveListPokemon: (pokemon: PokemonDTO) => void;
-  handleAddListSearchPokemon: (pokemon: PokemonDTO) => void;
-  handleLikedPokemon: (id: number) => void;
-  handleDisLikedPokemon: (id: number) => void;
+interface RegisterData {
+  
 }
 
-export const PokemonContext = createContext<PokemonData>({} as PokemonData);
+export const RegisterContext = createContext<RegisterData>({} as RegisterData);
 
-export function PokemonProvider({ children }: PokemonProviderProps) {
-  const [listPokemon, setListPokemon] = useState<PokemonDTO[]>([]);
-  const [listLikedPokemon, setListLikedPokemon] = useState<PokemonDTO[]>([]);
-  const [listSearchPokemon, setListSearchPokemon] = useState<PokemonDTO[]>([]);
+export function RegisterProvider({ children }: RegisterProviderProps) {
 
-  function handleSetListPokemon(list: PokemonDTO[]) {
-    setListPokemon(list);
-  }
 
-  function handleSetLisSearchPokemon(list: PokemonDTO[]) {
-    setListSearchPokemon(list);
-  }
-
-  function handleAddListPokemon(pokemon: PokemonDTO) {
-    setListPokemon((oldArray) => [...oldArray, pokemon]);
-  }
-
-  function handleAddListLikedPokemon(pokemon: PokemonDTO) {
-    setListLikedPokemon((oldArray) => [...oldArray, pokemon]);
-  }
-
-  function handleAddListSearchPokemon(pokemon: PokemonDTO) {
-    setListSearchPokemon((oldArray) => [...oldArray, pokemon]);
-  }
-
-  function handleRemoveListPokemon(pokemon: PokemonDTO) {
-    const newList = listLikedPokemon.filter((item) => pokemon.id !== item.id);
-    setListLikedPokemon(newList);
-  }
-
-  function handleLikedPokemon(id: number) {
-    const newList = listPokemon.map((item) => {
-      if (id === item.id) {
-        const pokemon = {
-          ...item,
-          isFavorite: true,
-        };
-        handleAddListLikedPokemon(pokemon);
-        return pokemon;
-      } else {
-        return item;
-      }
-    });
-
-    const newListSerch = listSearchPokemon.map((item) => {
-      if (id === item.id) {
-        const pokemon = {
-          ...item,
-          isFavorite: true,
-        };
-        handleAddListLikedPokemon(pokemon);
-        return pokemon;
-      } else {
-        return item;
-      }
-    });
-
-    setListPokemon(newList);
-    setListSearchPokemon(newListSerch);
-  }
-
-  function handleDisLikedPokemon(id: number) {
-    const newList = listPokemon.map((item) => {
-      if (id === item.id) {
-        const pokemon = {
-          ...item,
-          isFavorite: false,
-        };
-        handleRemoveListPokemon(pokemon);
-        return pokemon;
-      } else {
-        return item;
-      }
-    });
-
-    const newListSerch = listSearchPokemon.map((item) => {
-      if (id === item.id) {
-        const pokemon = {
-          ...item,
-          isFavorite: false,
-        };
-        handleRemoveListPokemon(pokemon);
-        return pokemon;
-      } else {
-        return item;
-      }
-    });
-    setListSearchPokemon(newListSerch);
-    setListPokemon(newList);
-  }
 
   return (
-    <PokemonContext.Provider
+    <RegisterContext.Provider
       value={{
-        listPokemon,
-        listLikedPokemon,
-        listSearchPokemon,
-        handleSetListPokemon,
-        handleAddListPokemon,
-        handleRemoveListPokemon,
-        handleDisLikedPokemon,
-        handleLikedPokemon,
-        handleAddListSearchPokemon,
+        
       }}
     >
       {children}
-    </PokemonContext.Provider>
+    </RegisterContext.Provider>
   );
 }
 
-export function usePokemons() {
-  const context = useContext(PokemonContext);
+export function useRegisters() {
+  const context = useContext(RegisterContext);
   return context;
 }
